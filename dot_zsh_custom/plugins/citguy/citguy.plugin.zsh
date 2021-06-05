844581dd959e9df10da90c706d0309d503f873bb
Ryan Johnson <rhino.citguy@gmail.com>
2021-06-04 20:47:30 -0500

    migrate and cleanup shell configs

--- dot_zsh_custom/plugins/citguy/citguy.plugin.zsh
+++ dot_zsh_custom/plugins/citguy/citguy.plugin.zsh
@@ -1,21 +1,34 @@
-export EDITOR='vim'
-export PATH=$PATH:$HOME/bin
+#========================================#
+# ENVIRONMENT VARIABLES
+#========================================#
+export PATH=$PATH:$HOME/bin # TODO: only modify if `$HOME/bin` is NOT in $PATH
+export CHEZMOI_PATH="~/.local/share/chezmoi"
+export EDITOR="vim"
 export SSH_KEY_PATH="~/.ssh/id_rsa"
 
+
+#========================================#
+# ALIASES
+#========================================#
 alias ls="ls -hFTG"
 alias :q="exit"
 alias env="env | sort"
 alias be='PRY_AP=0 bundle exec'
 alias cm="chezmoi"
+alias :dotfiles="cd $CHEZMOI_PATH"
 
 
-## nodenv config
+#========================================#
+# INITIALIZATION
+#========================================#
+
+# nodenv config
 if which nodenv > /dev/null
 then
   eval "$(nodenv init -)";
 fi
 
-## rbenv config
+# rbenv config
 if which rbenv > /dev/null
 then
   #export PATH=$HOME/.rbenv/bin:$PATH
--- shells/bash/.bash_aliases
+++
@@ -1,7 +0,0 @@
-# some more ls aliases
-alias :q='exit'
-alias env='env | sort'
-alias be='PRY_AP=0 bundle exec'
-alias ls='ls -hFT'
-
-alias gitk='"$(brew --prefix tcl-tk)/bin/wish" "$(which gitk)"'
